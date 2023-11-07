a30982f38b2d6f88763422850e3b631460485201
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-11-07 06:37:43 +0300

    save 07/11/23

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import Classes.Market;
+import Classes.OrdinaryClient;
+import Classes.SpecialClient;
+import Classes.TaxInspector;
+import Interfaces.iActorBehaviour;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Market magnit = new Market();
+
+        iActorBehaviour client1 = new OrdinaryClient("Николай");
+        iActorBehaviour client2 = new SpecialClient("Владимир", 1);
+        iActorBehaviour client3 = new TaxInspector();
+        iActorBehaviour client4 = new PromoClient("Дмитрий", 1, "Чай со слоном");
+        iActorBehaviour client5 = new PromoClient("Альберт", 2, "Чай со слоном");
+
+        magnit.acceptToMarket(client1);
+        magnit.acceptToMarket(client2);
+        magnit.acceptToMarket(client3);
+        magnit.acceptToMarket(client4);
+        magnit.acceptToMarket(client5);
+        magnit.update();
+    }
+}
---
+++ src/Interfaces/iMarketBehaviour.java
@@ -0,0 +1,27 @@
+package Interfaces;
+
+
+import Classes.Actor;
+
+import java.util.List;
+
+/**
+ * @apiNote Интерфейс для описания посещения магазина
+ */
+public interface iMarketBehaviour {
+
+    /**
+     * @apiNote метод регистрации посетителя при входе в магазин
+     */
+    void acceptToMarket(iActorBehaviour actor);
+
+    /**
+     * @apiNote метод регистрации посетителя при выходе из магазина
+     */
+    void releaseFromMarket(List<Actor> actors);
+
+    /**
+     * @apiNote метод обновления данных о состоянии посетителя
+     */
+    void update();
+}
